ad97b461a4d7fefc2e38ba283a0dcfac993a8809
shivakrishna <shivakrishnagodugu@outlook.com>
2023-08-23 18:47:33 +0530

    mongo.repo added

--- Catalogue.sh
+++ Catalogue.sh
@@ -84,6 +84,6 @@ yum install mongodb-org-shell -y &>>$LOGFILE
 
 VALIDATE $? "Installing mongo client"
 
-mongo --host mongodb.joindevops.online </app/schema/catalogue.js &>>$LOGFILE
+mongo --host mongodb.varahifarms.online </app/schema/catalogue.js &>>$LOGFILE
 
 VALIDATE $? "loading catalogue data into mongodb"
